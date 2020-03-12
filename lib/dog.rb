30d79171a61a15d4ec47c335e173108bc164e74b
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-12 18:19:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,82 +1,32 @@
 # Add your code here
-class Owner
-OWNERS = []
-attr_accessor :name, :pets
-attr_reader :species
+class Dog
+  @@all = []
 
-def initialize(species)
-  @species = species
-  OWNERS << self
-  @pets = {:fishes => [], :dogs => [], :cats => []}
-end
-
-def self.all ##Do we have to use this if we use a class variable? @@all
-OWNERS
-end
-
-def self.reset_all
-OWNERS.clear
-end
-
-def self.count
-OWNERS.size
-end
-
-def species
-  @species
-end
-
-def say_species
-  return "I am a #{species}"
-end
-
-def name
-  @name #where did we set @name = name?
-end
-
-def buy_fish(name)
-  pets[:fishes] << Fish.new(name) #how does owner.rb know where fish.rb is?
-end
-
-def buy_cat(name)
-  pets[:cats] << Cat.new(name)
-end
-
-def buy_dog(name)
-  pets[:dogs] << Dog.new(name)
-end
-
-def walk_dogs
-  pets[:dog].each do |dog|
-    dog.mood = "happy"
+  def initialize(name)
+    @name = name
+    self.save
   end
-end
 
-def play_with_cats
-  pets[:cats].each do |cat|
-  cat.mood = "Happy"
+  def self.all
+    @@all
   end
-end
 
-def feed_fish
-  pets[:fish].each do |fish|
-  fish.mood = "happy"
+  def self.clear_all
+    @@all = [] 
   end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
 end
 
-def list_pets
-  "I have #{pets[:fishes].count} fish. I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
-end
+  def save
+    @@all << self
+  end
 
-end #finish Owner
-
-class Fish
-attr_reader :name
-attr_accessor :mood
-
-def initialize
-  @name = name
-  @mood = "nervous"
-end
+  def name
+    @name
+  end
 
 end 
