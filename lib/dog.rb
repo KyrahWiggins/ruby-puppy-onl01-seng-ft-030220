6ec922a2996b2cc31eb8235397f5ee8266d4dde4
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-12 18:13:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,82 +1,82 @@
 # Add your code here
 class Owner
-OWNERS = [] 
-attr_accessor :name, :pets 
-attr_reader :species 
+OWNERS = []
+attr_accessor :name, :pets
+attr_reader :species
 
 def initialize(species)
-  @species = species 
-  OWNERS << self 
+  @species = species
+  OWNERS << self
   @pets = {:fishes => [], :dogs => [], :cats => []}
-end 
-
-def self.all ##Do we have to use this if we use a class variable? @@all 
-OWNERS 
-end 
-
-def self.reset_all 
-OWNERS.clear 
-end 
-
-def self.count
-OWNERS.size 
 end
 
-def species 
-  @species 
-end 
+def self.all ##Do we have to use this if we use a class variable? @@all
+OWNERS
+end
 
-def say_species 
+def self.reset_all
+OWNERS.clear
+end
+
+def self.count
+OWNERS.size
+end
+
+def species
+  @species
+end
+
+def say_species
   return "I am a #{species}"
-end 
-  
-def name 
-  @name #where did we set @name = name? 
-end 
+end
+
+def name
+  @name #where did we set @name = name?
+end
 
 def buy_fish(name)
-  pets[:fishes] << Fish.new(name) #how does owner.rb know where fish.rb is? 
-end 
+  pets[:fishes] << Fish.new(name) #how does owner.rb know where fish.rb is?
+end
 
 def buy_cat(name)
   pets[:cats] << Cat.new(name)
-end 
+end
 
 def buy_dog(name)
   pets[:dogs] << Dog.new(name)
-end 
+end
 
 def walk_dogs
   pets[:dog].each do |dog|
     dog.mood = "happy"
-  end 
-end 
+  end
+end
 
 def play_with_cats
-  pets[:cats].each do |cat| 
+  pets[:cats].each do |cat|
   cat.mood = "Happy"
-  end 
-end 
+  end
+end
 
 def feed_fish
   pets[:fish].each do |fish|
   fish.mood = "happy"
-  end 
-end 
+  end
+end
 
 def list_pets
   "I have #{pets[:fishes].count} fish. I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
-end 
+end
 
-end #finish Owner 
+end #finish Owner
 
-class Fish 
-attr_reader :name 
-attr_accessor :mood 
+class Fish
+attr_reader :name
+attr_accessor :mood
 
-def initialize 
-  @name = name 
+def initialize
+  @name = name
   @mood = "nervous"
-end 
+end
 
 end 
